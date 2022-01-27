218c9182a839752023d203a5becacb1c64579189
Scott Giese <scttgs0@gmail.com>
2022-01-27 14:37:42 -0600

    assemble script for Intel hex format

---
+++ make_hex.sh
@@ -0,0 +1,10 @@
+
+64tass  --m65816 \
+        --long-address \
+        --flat \
+        -b \
+        --intel-hex \
+        -o fortapoc.hex \
+        --list=fortapocalypse.lst \
+        --labels=fortapocalypse.lbl \
+        fort.s
