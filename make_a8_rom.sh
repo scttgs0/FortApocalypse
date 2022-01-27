1d6e0f7b046f782c8e140fe778003f4ab01fb307
Scott Giese <scttgs0@gmail.com>
2022-01-27 12:43:50 -0600

    build script for ROM generation

---
+++ make_a8_rom.sh
@@ -0,0 +1,8 @@
+
+64tass  --m65xx \
+        --atari-xex \
+        -b \
+        -o fortapocalypse.rom \
+        --list=fortapocalypse_a8_rom.lst \
+        --labels=fortapocalypse_a8_rom.lbl \
+        fort.s
